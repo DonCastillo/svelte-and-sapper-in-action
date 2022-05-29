6527bb2451f0dceb204e6f1824e22a69388530ed
DonCastillo <don.qcastillo@yahoo.com>
2022-05-28 18:21:10 -0600

    added hello

---
+++ hello/src/Clock.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let color = 'blue';
+	let hhmmss = '';
+	setInterval(() => {
+		hhmmss = new Date().toLocaleTimeString();
+	}, 1000);
+</script>
+
+<div>
+	I will be a clock.
+</div>
+<span style="color: {color}">{hhmmss}</span>
